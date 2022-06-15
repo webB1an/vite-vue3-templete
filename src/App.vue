bc269842cdd70abf0c4c31550a3a2271ec93ad30
webB1an <webB1anyaoyao@gmail.com>
2022-06-15 17:04:10 +0800

    feat: add svg component example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />  
+  <!-- icons/0.svg -->
+  <svg-icon name="0" />
+  <!-- icons/svg/0.svg -->
+  <svg-icon name="svg-0" />
+  <svg-icon name="svg-1" />
+  <svg-icon name="svg-2" />
+  <router-view />
 </template>
 
 <script setup lang="ts">
